a9172c5e9e56acdab1c289ccaa7a612e370f3c50
mitchellhein25 <mitchell.d.hein@gmail.com>
2025-05-31 13:09:25 -0500

    Installed DaisyUi and refactored NavBar and CashflowSideBar to use DaisyUI stylings.

--- frontend/src/app/ui/components/cashflow/CashflowSideBar.tsx
+++ frontend/src/app/ui/components/cashflow/CashflowSideBar.tsx
@@ -20,25 +20,16 @@ export default function CashflowSideBar() {
             const isActive = pathname.endsWith(item.href)
             
             return (
-            <div 
-                key={item.href}
-                className={`px-4 py-2 rounded text-sm font-medium transition-colors ${
-                isActive 
-                    ? 'bg-nav-item-active-bg-light dark:bg-nav-item-active-bg-dark' 
-                    : ''
-                }`}
-            >
-                <Link 
-                href={item.href}
-                className={
-                    isActive
-                    ? 'text-nav-item-active-light dark:text-nav-item-active-dark'
-                    : 'text-nav-item-light dark:text-nav-item-dark hover:text-nav-item-hover-light dark:hover:text-nav-item-hover-dark transition-colors'
-                }
-                >
-                {item.label}
-                </Link>
-            </div>
+              <Link 
+              href={item.href}
+              className={
+                  isActive
+                  ? 'btn btn-primary mx-5'
+                  : 'btn btn-ghost mx-5'
+              }
+              >
+              {item.label}
+              </Link>
             )
         })}
     </div>
