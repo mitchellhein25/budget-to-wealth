5def7a85a1c9dbed2646bdd45f3abb1be0a571dd
mitchellhein25 <mitchell.d.hein@gmail.com>
2025-09-14 13:54:11 -0500

    Refactor ESLint configuration for improved clarity and maintainability
    - Simplified the ESLint configuration by removing unnecessary imports and restructuring the export format.
    - Enhanced compatibility with Next.js by utilizing FlatCompat for extending configurations, ensuring better integration with core web vitals and TypeScript.
    
    These changes contribute to a more streamlined and efficient linting setup in the project.

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,17 +1,13 @@
-import { defineConfig } from "eslint/config";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc'
+ 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default defineConfig([{
-    extends: compat.extends("next/core-web-vitals", "next/typescript"),
-}]);
+  baseDirectory: import.meta.dirname,
+})
+ 
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+  }),
+]
+ 
+export default eslintConfig
